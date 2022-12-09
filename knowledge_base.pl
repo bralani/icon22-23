de17df1e93c19860115843b3c3cf7a783cf4bc0e
Matteo Balice <matteobalice02@gmail.com>
2022-12-09 23:50:48 +0100

    meta-interpreti

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,5 +1,14 @@
-corso_uguale(E1, E2):- C1=C2, corso(E1, C1), corso(E2, C2).
+%%	Definizione meta-interpreti utilizzati nel progetto
 
-corso(analisi, informatica).
-corso(programmazione, informatica).
-corso(analisi2, matematica).
+% Vanilla
+prove(true).
+prove(oand(A,B)) :- prove(A), prove(B).
+prove(A) :- clausola(A,B), prove(B).
+
+% Profondità limitata
+bprove(true, D).
+bprove(oand(A,B), D) :- bprove(A,D), bprove(B,D).
+bprove(H, D) :- D >= 0, D1 is D-1, clausola(H,B), bprove(B,D1).
+
+
+%%  Regole della base di conoscenza
