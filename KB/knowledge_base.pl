107493df866487f1c82ee66dda68c84de859776e
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-12-20 14:49:11 +0100

    lunghezza_strada prolog

--- KB/knowledge_base.pl
+++ KB/knowledge_base.pl
@@ -11,16 +11,19 @@
  * @param L: lunghezza della strada (viene restituito il risultato)
  */
 lunghezza_strada(X, [Y,Z|[]], L) :- prop(X, type, strada), somma_nodi(Y, Z, L).
-%lunghezza_strada(X, [testa|coda], L) :- prop(X, type, strada), lunghezza is testa + lunghezza_strada(X, coda, L), L is lunghezza.
+lunghezza_strada(X, [T|[S|C]], L) :- prop(X, type, strada),
+                        somma_nodi(T, S, TR),
+                        lunghezza_strada(X, [S|C], LN),
+                        L is TR + LN.
+
 
 somma_nodi(X, Y, S) :- prop(X, latitudine, L1), prop(Y, latitudine, L2), 
                        prop(X, longitudine, G1), prop(Y, longitudine, G2), 
                        S is abs(L1 - L2 + G1 - G2).
 
-
 % Esempio
 prop(via_giordano_bruno, type, strada_primaria).
-prop(via_giordano_bruno, nodi, [nodo_1, nodo_2, nodo_3]).
+prop(via_giordano_bruno, nodi, [nodo_1, nodo_2, nodo_3, nodo_4]).
 
 prop(nodo_1, latitudine, 45.0).
 prop(nodo_1, longitudine, 9.0).
@@ -28,5 +31,8 @@ prop(nodo_1, longitudine, 9.0).
 prop(nodo_2, latitudine, 50.0).
 prop(nodo_2, longitudine, 9.0).
 
-prop(nodo_3, latitudine, 60.0).
+prop(nodo_3, latitudine, 55.0).
 prop(nodo_3, longitudine, 9.0).
+
+prop(nodo_4, latitudine, 60.0).
+prop(nodo_4, longitudine, 9.0).
--- KB/knowledge_base.py
+++ KB/knowledge_base.py
@@ -4,5 +4,5 @@ prolog = Prolog()
 
 prolog.consult("KB/knowledge_base.pl")
 
-for soln in prolog.query("lunghezza_strada(via_giordano_bruno, [nodo_1, nodo_2], L)"):
+for soln in prolog.query("lunghezza_strada(via_giordano_bruno, [nodo_1, nodo_4, nodo_3], L)"):
     print(soln["L"])
